0cc77dc7e6049261d35588ab47711fd5599a4cf0
Kirill Alekhnovich <kirillalekhnovich@gmail.com>
2022-12-28 03:10:04 +0100

    Unused dependencies removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,15 +17,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.security:spring-security-test")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.vladmihalcea:hibernate-types-52:1.0.0")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
