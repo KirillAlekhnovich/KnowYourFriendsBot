e5bb1e9ca1fce35327efc6084963e8b4bd7a9cc2
Kirill Alekhnovich <kirillalekhnovich@gmail.com>
2023-03-17 12:55:32 +0100

    RunApplication import added

--- client/telegram/src/main/kotlin/com/telegram/bot/TelegramBot.kt
+++ client/telegram/src/main/kotlin/com/telegram/bot/TelegramBot.kt
@@ -1,10 +1,11 @@
 package com.telegram.bot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class TelegramBot
 
 fun main(args: Array<String>) {
-    org.springframework.boot.runApplication<TelegramBot>(*args)
+    runApplication<TelegramBot>(*args)
 }
